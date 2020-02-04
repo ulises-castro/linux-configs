063af791c63294381428c631b7ba2305822d6deb
Ulises Castro <ulisescastro2015@gmail.com>
2020-02-04 17:07:02 -0600

    Added indicator to battery indicator

--- battery-indicator.sh
+++ battery-indicator.sh
@@ -1,3 +1,8 @@
-#!/bin.bash
+#!/bin bash
+batteryState="$(acpi | awk '{ print $3 }')"
+batteryPorcentage="$(acpi | awk '{ print $4 }')"
 
-echo Hello
+# Check if battery is discharging and has less than 26%
+if [ $batteryState == 'Discharging,' ] && [[ ${batteryPorcentage:0:2} -lt 26 ]]; then
+  notify-send --icon=/home/alex/.icons/la-capitaine-icon-theme-0.6.1/devices/scalable/battery.svg "Battery indicator" "You have to plug it your laptop"
+fi
