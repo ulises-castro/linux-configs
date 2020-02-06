302a3229e1c392b124a03c5e5f162b5d2abdc03e
Ulises Castro <ulisescastro2015@gmail.com>
2020-02-05 23:55:21 -0600

    Alert user when he already gets 80

--- battery-indicator.sh
+++ battery-indicator.sh
@@ -6,3 +6,8 @@ batteryPorcentage="$(acpi | awk '{ print $4 }')"
 if [ $batteryState == 'Discharging,' ] && [[ ${batteryPorcentage:0:2} -lt 26 ]]; then
   notify-send --icon=/home/alex/.icons/la-capitaine-icon-theme-0.6.1/devices/scalable/battery.svg "Battery indicator" "You have to plug it your laptop"
 fi
+
+# Check if battery is chargin and it at least 80%
+if [ $batteryState == 'Charging,' ] && [[ ${batteryPorcentage:0:2} -gt 79 ]]; then
+  notify-send --icon=/home/alex/.icons/la-capitaine-icon-theme-0.6.1/devices/scalable/battery.svg "Battery indicator" "You have to disconnect your laptop"
+fi
